1ed23fbb3ce15a9867aa77e31fb6c69ba1e17564
Koske Kimutai <koskedk@live.com>
2023-02-04 17:04:27 +0300

    ui samples

--- src/LiveClinic/ClientApp/src/features/registry/components/patients/PatientList.tsx
+++ src/LiveClinic/ClientApp/src/features/registry/components/patients/PatientList.tsx
@@ -4,6 +4,8 @@ import {DataTable} from "primereact/datatable";
 import {Column} from "primereact/column";
 import {Button} from "primereact/button";
 import {useNavigate} from "react-router-dom";
+import {SplitButton} from "primereact/splitbutton";
+import {Toolbar} from "primereact/toolbar";
 
 interface Props {
     patients?: Patient[];
@@ -11,6 +13,14 @@ interface Props {
 
 const PatientList: FC<Props> = ({patients}) => {
     const navigate = useNavigate();
+
+
+    const startContent = (
+        <React.Fragment>
+            <Button label="Register" icon="pi pi-plus" className="mr-2"/>
+        </React.Fragment>
+    );
+
     const openTemplate = (rowData: any, column: any) => {
         return (
             <div>
@@ -24,6 +34,9 @@ const PatientList: FC<Props> = ({patients}) => {
 
     return (
         <div>
+            <div className="card">
+                <Toolbar start={startContent} />
+            </div>
             <DataTable value={patients} responsiveLayout="scroll">
                 <Column field="memberNo" header="No"></Column>
                 <Column field="patientName.lastName" header="Last Name"></Column>
--- src/LiveClinic/ClientApp/src/features/registry/components/patients/PatientProfile.tsx
+++ src/LiveClinic/ClientApp/src/features/registry/components/patients/PatientProfile.tsx
@@ -1,37 +1,53 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {Patient} from "../../models/patient";
 import {useQuery} from "react-query";
 import {patientService} from "../../services/patient-service";
-import {Fieldset} from "primereact/fieldset";
 import {useParams} from "react-router-dom";
+import {useForm} from "react-hook-form";
+import {Card} from "primereact/card";
 
+type FormAction= {
+    title?: string
+    showEdit?:boolean
+    showSave?:boolean
+}
 const PatientProfile: FC = () => {
 
+    const [action,setAction]=useState<FormAction>({
+        title: 'Register New',
+        showEdit: false,
+        showSave: true
+    });
     const {patientId} = useParams()
-
     const {isLoading, error, data} = useQuery<Patient, Error>('patient', async () => patientService.getById(patientId));
+
+    useEffect(() => {
+       if (data?.id){
+           setAction(prevState => ({
+               ...prevState,title:'View/Edit Patient'
+           }))
+       }
+    }, []);
 
     if (isLoading) return (<h1>Loading...</h1>)
 
     if (error) return <h1>`An error has occurred: ${error.message}`</h1>
 
-    const legendTemplate = (
-        <div className="flex align-items-center text-primary">
-            <span className="pi pi-user mr-2"></span>
-            <span
-                className="font-bold text-lg"> {`.  ${data?.patientName?.lastName} ${data?.patientName?.firstName}  | ${data?.memberNo}`}</span>
-        </div>
-    );
-
     return (
-        <div className="card">
-            <Fieldset legend={legendTemplate}>
-                <>
-                    <p><>DOB:{data?.birthDate}</></p>
-                    <p><>Gender:{data?.gender}</></p>
-                </>
-            </Fieldset>
-        </div>
+        <Card title={action.title}>
+            <p>Hey</p>
+            {/*<form onSubmit={handleSubmit(onSubmit)}>*/}
+            {/*    /!* register your input into the hook by invoking the "register" function *!/*/}
+            {/*    <input defaultValue="test" {...register("example")} />*/}
+
+            {/*    /!* include validation with required or other standard HTML validation rules *!/*/}
+            {/*    <input {...register("exampleRequired", { required: true })} />*/}
+            {/*    /!* errors will return when field validation fails  *!/*/}
+            {/*    {errors.exampleRequired && <span>This field is required</span>}*/}
+
+            {/*    <input type="submit" />*/}
+            {/*</form>*/}
+        </Card>
     );
 }
 export default PatientProfile;
